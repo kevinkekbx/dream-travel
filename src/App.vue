274b35041e34a978b2f749d96740a15b8c15bbb6
Chris <hizyyv@gmail.com>
2023-11-15 15:47:13 +0800

    feat: autoSwitchDark

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+autoSwitchDark()
+</script>
+
 <template>
   <main p="x-4 y-10" text="center gray-700 dark:gray-200">
     <RouterView />
--- src/composables/dark.ts
+++ src/composables/dark.ts
@@ -1,2 +1,20 @@
 export const isDark = useDark()
 export const toggleDark = useToggle(isDark)
+
+/**
+ * 晚上 8 点到早上 6 点准点自动切换为暗色模式
+ *
+ * 30 分钟检查一次
+ */
+export function autoSwitchDark() {
+  const hour = new Date().getHours()
+  const _isDark = hour >= 20 || hour < 6
+
+  if (hour === 6 && _isDark)
+    isDark.value = false
+
+  if (hour === 20 && !_isDark)
+    isDark.value = true
+
+  setTimeout(autoSwitchDark, 1000 * 60 * 30)
+}
