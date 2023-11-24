55e8aba30cf6fbb1c0899a85659d8898d009c5c1
Chris <hizyyv@gmail.com>
2023-11-24 16:20:20 +0800

    chore: update

--- src/pages/config.vue
+++ src/pages/config.vue
@@ -1,10 +1,25 @@
 <script lang='ts' setup>
+import qs from 'query-string'
+import { showSuccessToast, showToast } from 'vant'
+
 const route = useRoute()
 const { type = 'SIGN_UP' } = route.query
 
 // 报名审核 SIGN_UP
 // 梦想基金审核 DREAM_FUND
 // 试炼审核 TRIAL
+
+interface User {
+  age: number
+  createDate: number
+  dreamCar: string
+  dreamerType: string
+  email: string
+  goldenIdea: string
+  id: number
+  nickName: string
+  phone: string
+}
 
 const typeEnum = ref({
   SIGN_UP: '报名审核',
@@ -14,6 +29,42 @@ const typeEnum = ref({
 const _type = type as keyof typeof typeEnum
 // @ts-expect-error something wrong
 const title = computed(() => typeEnum.value[_type])
+
+const list = ref<User[]>([])
+
+async function fetchSignUpList() {
+  const { data } = await useFetch('http://150.158.43.244:8080/dreamer/list?page=0&size=0').post().json()
+  if (data.value)
+    list.value = data.value.data.list
+}
+
+onMounted(async () => {
+  if (type === 'SIGN_UP')
+    fetchSignUpList()
+  else if (type === 'DREAM_FUND')
+    fetchSignUpList()
+  else if (type === 'TRIAL')
+    fetchSignUpList()
+})
+
+function rejectIt(u: User) {
+  // eslint-disable-next-line no-console
+  console.log('rejectIt', u)
+}
+
+async function resolveIt(u: User) {
+  const model = {
+    dreamerId: u.id,
+    status: 0,
+  }
+  const url = `http://150.158.43.244:8080/dream/process/add?${qs.stringify(model)}`
+  const { data } = await useFetch(url).post().json()
+  if (data.value.code === 200) {
+    showSuccessToast('操作成功')
+    fetchSignUpList()
+  }
+  else { showToast(data.value.message) }
+}
 </script>
 
 <template>
@@ -24,29 +75,28 @@ const title = computed(() => typeEnum.value[_type])
       {{ title }}
     </h3>
     <div mt-7.5 fccc gap-3>
-      <div v-for="i in 6" :key="i" rd-1 bg-white p-4>
+      <div v-for="u in list" :key="u.id" w-full rd-1 bg-white p-4>
         <div fbc text-lg fw-700>
           <div>
-            <span>zahnasd</span>
+            <span>{{ u.nickName }}</span>
             <span mx-1 fw-normal>|</span>
-            <span>zahnasd</span>
+            <span>{{ u.dreamerType }}</span>
           </div>
-          <span>梦想车型： 718</span>
+          <span>梦想车型： {{ u.dreamCar }}</span>
         </div>
         <div op-50>
-          asddasdasd@.com
+          {{ u.email }}
           <br>
-          13131-1-3-1-
+          {{ u.phone }}
         </div>
         <div my-4 w-full flex>
-          x啊是妇女节卡上飞机卡福卡上净空法师的减肥健身的进口关税的健康
-          x啊是妇女节卡上飞机卡福卡上净空法师的减肥健身的进口关税的健康
+          {{ u.goldenIdea }}
         </div>
         <div fcc gap-2>
-          <van-button color="#040609" square plain block type="primary">
+          <van-button color="#040609" square plain block type="primary" @click="rejectIt(u)">
             忽略
           </van-button>
-          <van-button color="#040609" square block type="primary">
+          <van-button color="#040609" square block type="primary" @click="resolveIt(u)">
             通过
           </van-button>
         </div>
@@ -54,6 +104,3 @@ const title = computed(() => typeEnum.value[_type])
     </div>
   </div>
 </template>
-
-<style lang='scss' scoped>
-</style>
